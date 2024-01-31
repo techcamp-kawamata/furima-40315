e6e5e0fda6a5450e8ad5df0e1770f824b09d7f18
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-31 23:58:24 +0900

    Rubocopを実行

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  
-  #has_many :items
-  #has_many :purchases
-  
+
+  # has_many :items
+  # has_many :purchases
+
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: "全角（漢字・ひらがな・カタカナ）での入力が必須です" }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: "全角（漢字・ひらがな・カタカナ）での入力が必須です" }
-    validates :last_name_kana, format: { with: /\p{katakana}/, message: "全角（カタカナ）での入力が必須です" }
-    validates :first_name_kana, format: { with: /\p{katakana}/, message: "全角（カタカナ）での入力が必須です" }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+    validates :last_name_kana, format: { with: /\p{katakana}/, message: '全角（カタカナ）での入力が必須です' }
+    validates :first_name_kana, format: { with: /\p{katakana}/, message: '全角（カタカナ）での入力が必須です' }
     validates :birthday
   end
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: "半角英数字混合で、6文字以上で入力してください" }
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: '半角英数字混合で、6文字以上で入力してください' }
 end
