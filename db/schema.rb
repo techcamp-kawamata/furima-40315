1f3277ebd1a26c843d77a614ce9507c12ea069fe
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-31 14:46:41 +0900

    ユーザー管理機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,14 @@
 
 ActiveRecord::Schema[7.0].define(version: 2024_01_29_144344) do
   create_table "users", charset: "utf8", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthday", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
