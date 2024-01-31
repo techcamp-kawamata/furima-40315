3acae279aca44027ef5fb5532ff2767c9b945f50
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-01-31 21:25:43 +0900

    テストコードの記入

---
+++ spec/factories/users.rb
@@ -0,0 +1,13 @@
+FactoryBot.define do
+  factory :user do
+    nickname              { Faker::Name.initials }
+    email                 { Faker::Internet.email }
+    password              { 'a1' + Faker::Internet.password(min_length: 4, mix_case: false) }
+    password_confirmation { password }
+    last_name             { 'やマ田' }
+    first_name            { '太ろウ' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birthday              { Faker::Date.between(from: '1930-01-01', to: '2017-12-31') }
+  end
+end
