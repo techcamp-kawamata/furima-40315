cf3c2448bdc2ab8db5f2fa312ad0efbed4d95140
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-02-01 17:48:42 +0900

    バリデーションの記入(途中)

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,15 @@ class Item < ApplicationRecord
   belongs_to: user
   has_one_attached :image
 
+  with_options presence: true do
+    validates :item_name
+    validates :detail
+    validates :category_id
+    validates :condition_id
+    validates :postage_id
+    validates :region_id
+    validates :delivery_time_id
+    validates :price
+    validates :user
+  end
 end
