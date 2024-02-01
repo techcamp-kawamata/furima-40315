d5f7c3d5e3e383e63e9449d4dd533fcbee9997a1
techcamp-kawamata <155680479+techcamp-kawamata@users.noreply.github.com>
2024-02-01 16:26:43 +0900

    ご指摘いただいた箇所の修正

--- app/controllers/users_controller.rb
+++
@@ -1,5 +0,0 @@
-class UsersController < ApplicationController
-  def index
-    @users = user.find(params[:id])
-  end
-end
--- app/models/user.rb
+++ app/models/user.rb
@@ -9,11 +9,12 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+    validates :last_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
+    validates :first_name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角（漢字・ひらがな・カタカナ）での入力が必須です' }
     validates :last_name_kana, format: { with: /\p{katakana}/, message: '全角（カタカナ）での入力が必須です' }
     validates :first_name_kana, format: { with: /\p{katakana}/, message: '全角（カタカナ）での入力が必須です' }
     validates :birthday
   end
-  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d).+\z/, message: '半角英数字混合で、6文字以上で入力してください' }
+  validates :password,
+            format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[^\p{Hiragana}\p{Katakana}\p{Han}]+\z/, message: '半角英数字混合で、6文字以上で入力してください' }
 end
